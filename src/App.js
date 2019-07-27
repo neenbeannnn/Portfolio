d2d31a29bd398a46fc5496060dd27b4ffcdb1e86
neenbeannnn <hmn.nini1970@gmail.com>
2019-07-27 12:37:21 -0700

    added navbar and portfolio elements

--- src/App.js
+++ src/App.js
@@ -9,18 +9,63 @@ class App extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      readMoreSpin1: false
+      readMoreSpin1: false,
+      readMoreSpin2: false,
+      readMoreSpin3: false,
+      readMoreSpin4: false,
+      readMoreSpin5: false,
+      menuClick: false
     }
+  }
+
+  handleMenuClick = () => {
+    this.refs.slide1.scrollIntoView();
+    this.setState({menuClick: !this.state.menuClick});
+  }
+
+  handleMenuProjectClick = () => {
+    this.setState({menuClick: false});
+    const timer = setTimeout(() => {
+      this.refs.slide2.scrollIntoView();
+    }, 1000);
+    return () => clearTimeout(timer);
+  }
+
+  handleMenuWorkClick = () => {
+    this.setState({menuClick: false});
+    const timer = setTimeout(() => {
+      this.refs.slide3.scrollIntoView();
+    }, 1000);
+    return () => clearTimeout(timer);
+  }
+
+  handleMenuConnectClick = () => {
+    this.setState({menuClick: false});
+    const timer = setTimeout(() => {
+      this.refs.slide4.scrollIntoView();
+    }, 1000);
+    return () => clearTimeout(timer);
   }
 
   render(){
     return (
       <Fragment>
         <div className='wrapper'>
-          <div className='slide-one'>
-
+          <div className={'overlay-menu ' + (this.state.menuClick ? ' open' : '')}>
+            <h3 className={'overlay-name text-center' + (this.state.menuClick ? ' open' : '')}>Nina M. Huang</h3>
+            <hr className={(this.state.menuClick ? ' open' : '')}/>
+            <h2 className={'overlay-projects text-center' + (this.state.menuClick ? ' open' : '')} onClick={this.handleMenuProjectClick}>projects.</h2> 
+            <h2 className={'overlay-work text-center' + (this.state.menuClick ? ' open' : '')} onClick={this.handleMenuWorkClick}>work.</h2>
+            <h2 className={'overlay-contact text-center' + (this.state.menuClick ? ' open' : '')} onClick={this.handleMenuConnectClick}>let's connect.</h2>     
+          </div>
+          <div className='slide-one' ref={'slide1'}>
+            <div className='menu-bar'>
+              <span className={'menu-container' + (this.state.menuClick ? ' clicked' : '')} onClick={this.handleMenuClick}>
+                <box-icon name='menu' color={'#F7FFFE'} size={'50px'}></box-icon>
+              </span>
+            </div>
             <div className='text-container'>
-              <box-icon id='sun' type='solid' name='sun' color={'#F7FFFE'} size={'50px'} onClick={this.handleSun}></box-icon>
+              <box-icon id='sun' type='solid' name='sun' color={'#F7FFFE'} size={'50px'}></box-icon>
               <h3>
                 Hello, I am Nina M. Huang<span className='dot-one'>.</span><span className='dot-two'>.</span><span className='dot-three'>.</span>
               </h3>
@@ -42,11 +87,11 @@ class App extends Component {
             <div className='slide-two-body'>
               <div className='project-container'>  
                 <div className='featured-project'>
-                  <h3 className='featured-project-title'>
+                  <h3 className='project-title'>
                     BirdWatchers iOS app
                   </h3>
                   <hr/>
-                  <h2 className='featured-project-info'>
+                  <h2 className='project-info'>
                     Coming into this hackathon, my partner and I wanted to incorporate our interests in coding 
                     and environmentalism into one cohesive app. We drew inspiration from the various 
                     species of birds found near the UCSB lagoon and we wanted to learn more about 
@@ -59,18 +104,57 @@ class App extends Component {
                 </div>
                 <Row>
                   <Col xs={6} className='project-one'>
-                  
+                    <h3 className='project-title'>
+                      LocoNote
+                    </h3>
+                    <hr/>
+                    <h2 className='project-info text-center'>
+                      Pin an emoji and short message in your current location 
+                      to be viewed by other users of the iOS app
+                    </h2>
+                    <div className={'read-more-container' + (this.state.readMoreSpin2 ? ' spin' : ' ')} onClick={() => this.setState({readMoreSpin2: true})} onAnimationEnd={() => this.setState({readMoreSpin2: false})}>
+                      <box-icon className='read-more' name='plus-circle' type='solid' size={'50px'} color={'#F7FFFE'}></box-icon>
+                    </div>
                   </Col>
                   <Col xs={6} className='project-two'>
-
+                    <h3 className='project-title'>
+                      Card Game
+                    </h3>
+                    <hr/>
+                    <h2 className='project-info text-center'>
+                      A simple 2-player card game that implements LinkedLists to remove matching cards
+                    </h2>
+                    <div className={'read-more-container' + (this.state.readMoreSpin3 ? ' spin' : ' ')} onClick={() => this.setState({readMoreSpin3: true})} onAnimationEnd={() => this.setState({readMoreSpin3: false})}>
+                      <box-icon className='read-more' name='plus-circle' type='solid' size={'50px'} color={'#F7FFFE'}></box-icon>
+                    </div>
                   </Col>
                 </Row>
                 <Row>
                   <Col xs={6} className='project-three'>
-                  
+                    <h3 className='project-title'>
+                      Bee World
+                    </h3>
+                    <hr/>
+                    <h2 className='project-info text-center'>
+                      Control a bee, collect honey from flowers, shoot honey at wasps, 
+                      and collect upgrades!
+                    </h2>
+                    <div className={'read-more-container' + (this.state.readMoreSpin4 ? ' spin' : ' ')} onClick={() => this.setState({readMoreSpin4: true})} onAnimationEnd={() => this.setState({readMoreSpin4: false})}>
+                      <box-icon className='read-more' name='plus-circle' type='solid' size={'50px'} color={'#F7FFFE'}></box-icon>
+                    </div>
                   </Col>
                   <Col xs={6} className='project-four'>
-
+                    <h3 className='project-title'>
+                      wikiWhat
+                    </h3>
+                    <hr/>
+                    <h2 className='project-info text-center'>
+                      A short fun simple game about matching absurd wikiHow photos
+                       with equally weird tutorial names.
+                    </h2>
+                    <div className={'read-more-container' + (this.state.readMoreSpin5 ? ' spin' : ' ')} onClick={() => this.setState({readMoreSpin5: true})} onAnimationEnd={() => this.setState({readMoreSpin5: false})}>
+                      <box-icon className='read-more' name='plus-circle' type='solid' size={'50px'} color={'#F7FFFE'}></box-icon>
+                    </div>
                   </Col>
                 </Row>
               </div>
@@ -108,13 +192,30 @@ class App extends Component {
 
           <div className='slide-four' ref={'slide4'}>
             <div className='title'>
-              Say hi!
+              Let's connect!
             </div>
             <div className='contact-box-container'>
               <div className='contact-box'>
                 <Row>
                   <Col xs={8} className='left-side'>
-                    
+                    <h3 className='email-text'>
+                      ninamckenzie88@
+                      <br/>gmail.com
+                    </h3>
+                    <hr/>
+                    <h2 className='description'>
+                      Hi! I'm Nina, a full-time student at UCSB and front-end developer based in Irvine. I love working with 
+                      kids and am constantly learning something new (currently React). Feel free to reach
+                       out and send me a quick email. Let's connect!
+                    </h2>
+                    <div>
+                      <a className='mr-2' href='https://github.com/neenbeannnn' rel='noopener noreferrer' target='_blank'>
+                        <box-icon name='github' type='logo' size={'40px'} color={'#F7FFFE'}></box-icon>
+                      </a>
+                      <a href='https://www.linkedin.com/in/nina-huang-708a41171/' rel='noopener noreferrer' target='_blank'>
+                        <box-icon name='linkedin-square' type='logo' size={'40px'} color={'#F7FFFE'}></box-icon>
+                      </a>
+                    </div>
                   </Col>
                   <Col xs={4} className='right-side'>
                     
