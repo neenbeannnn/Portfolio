c190c943198d8b770390cd18b1ce53ae0e0b5031
neenbeannnn <hmn.nini1970@gmail.com>
2019-08-25 18:09:03 -0700

    Updates

---
+++ precache-manifest.e48457c2b1c33ae7579ed1cdbc180db9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c921d7e9bc6651186b7a14863e0aac",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "7edbc6feefff690ea0e8",
+    "url": "/Portfolio/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "95df8d768555ca616f23",
+    "url": "/Portfolio/static/css/main.3d84235c.chunk.css"
+  },
+  {
+    "revision": "7edbc6feefff690ea0e8",
+    "url": "/Portfolio/static/js/2.aa8b0991.chunk.js"
+  },
+  {
+    "revision": "95df8d768555ca616f23",
+    "url": "/Portfolio/static/js/main.37b405e7.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  }
+]);
